b9f17c0cf2f67e14d39ad73a604a8b9c8e717913
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-03-31 14:38:35 +0500

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,8 +23,8 @@ const Header = () => {
                   <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip='text' >Faaiz Aslam</Text>
                   <Text color={isDark ? "gray.200" : "gray.500"}> A self-motivated programmer with a passion to <br/> learn new things by experimenting 🗣</Text>
                   <Button mt={8}  bgGradient='linear(to-r, cyan.400, blue.500, purple.600)' _hover={{bgGradient:"linear(to-r, cyan.400, blue.500, purple.600)"}} onClick={() =>
-                    window.open('https://www.upwork.com/freelancers/~01104855743c9c25e2')
-                  }>Hire Me</Button>
+                    mailto="faaizaslam75@live.com"
+                  }>Contact</Button>
 
               </Box>
               <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
